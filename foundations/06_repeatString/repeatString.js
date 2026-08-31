803eb06eb37c6769cc3cdcb5a4f8a5df0bff0d8b
Kyle Buckley <kbuck2290@gmail.com>
2026-08-31 17:00:12 -0400

    completed lesson 6

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,4 +1,23 @@
-const repeatString = function() {
+const repeatString = function(hey, amount) {
+    let result = ""; 
+    for (let i = 0; i < amount; i++) 
+    result += hey;
+    return result;  
+
+    repeatString("hello", 10); 
+
+    repeatString("hi", 1); 
+
+    repeatString("bye", 0);  
+
+    repeatString("hey", 1000 ); 
+
+    repeatString("don't use the built-in repeat method!", 1); 
+
+    repeatString("goodbye", -1); 
+
+    repeatString("", 10);
+
 
 };
 
