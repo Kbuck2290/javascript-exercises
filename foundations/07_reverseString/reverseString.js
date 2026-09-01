ef40e1e65eed8f91dfb15ad41a7b61c901ccc4b6
Kyle Buckley <kbuck2290@gmail.com>
2026-09-01 19:45:33 -0400

    Lesson 7 completed

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,4 +1,22 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let result = "" 
+    
+    for (let i = string.length -1; i >= 0; i--) { 
+     result += string[i]; 
+    }
+    return result; 
+
+    reverseString("hello"); 
+
+    reverseString("hello there"); 
+
+    reverseString('123! abc! Hello, Odinite.'); 
+
+    reverseString("");
+
+
+
+
 
 };
 
