5c697295aa43cf8d5d9309ec999dbde75992b97c
Kyle Buckley <kbuck2290@gmail.com>
2026-09-04 20:16:54 -0400

    Lesson 10 Completed

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,6 +1,19 @@
-const leapYears = function() {
+const leapYears = function(timePeriod) { 
+     if (timePeriod % 400 === 0) {
+        return true;
+    }
+    
+    if (timePeriod % 100 === 0) {
+        return false;
+    }
+    
+    if (timePeriod % 4 === 0) {
+        return true;
+    } else { 
+        return false;  
+    };    
 
-};
+}
 
 // Do not edit below this line
 module.exports = leapYears;
