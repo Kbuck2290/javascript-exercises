c4ec9ef4cc7299530136162aeb3d044b2a815569
Kyle Buckley <kbuck2290@gmail.com>
2026-09-07 17:45:49 -0400

    completed lesson 12

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,24 +1,51 @@
-const add = function() {
+const add = function(num1, num2) { 
+  return num1 + num2;
 	
 };
 
-const subtract = function() {
+const subtract = function(num1,num2) { 
+  return num1 - num2; 
 	
 };
 
-const sum = function() {
+const sum = function(numbers) { 
+  let result = 0;
+  
+  for (let i = 0; i < numbers.length; i++){  
+  result += numbers[i]
+  }
+ 
+  return result; 
 	
 };
 
-const multiply = function() {
+const multiply = function(numbers) {
+  let result = 1; 
+
+  for (let i = 0; i < numbers.length; i++) {
+    result *= numbers[i]
+  } 
+
+  return result; 
 
 };
 
-const power = function() {
+const power = function(x,y) {
+  let result = 1;
+  for (let i = 0; i < y; i++) {
+    result *= x
+  }
 	
+  return result;
 };
 
-const factorial = function() {
+const factorial = function(numbers) {
+  let result = 1; 
+  for (let i = numbers; i > 0; i--) {
+    result *= i
+  } 
+
+  return result;
 	
 };
 
