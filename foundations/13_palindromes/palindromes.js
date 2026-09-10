75a3a638485d8f4232fb15d73e844653292f0509
Kyle Buckley <kbuck2290@gmail.com>
2026-09-10 11:44:07 -0400

    Lesson 13 completed

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let reversedString = "";  
+    let cleanedString = string.toLowerCase();
+    cleanedString = cleanedString.replace(/[^a-z0-9]/g, ""); 
+    for (let i = cleanedString.length - 1; i >= 0; i--) {
+        reversedString += cleanedString[i]; 
+    }  
+    return cleanedString === reversedString;
 };
 
 // Do not edit below this line
