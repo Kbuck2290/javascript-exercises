415d3655d17fd10359ab5915555a55759b5c83e4
Kyle Buckley <kbuck2290@gmail.com>
2026-09-11 12:28:32 -0400

    Lesson 14 completed

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,4 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(num) { 
+    num = Number(num);
+    if (num === 0) {
+        return 0;
+    }  
+    if (num < 0) {
+        return "OOPS";
+    }
+    let a = 1; 
+    let b = 1;  
+    for ( let i = 0; i < num -2; i++) {
+    let next = a + b; 
+    a = b; 
+    b = next;  
+} return b;
 
 };
 
