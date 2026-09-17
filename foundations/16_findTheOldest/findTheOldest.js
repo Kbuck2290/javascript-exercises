9a4159f4a125e68e292a4759b2f3b6ace0e3b2db
Kyle Buckley <kbuck2290@gmail.com>
2026-09-17 16:36:51 -0400

    Lesson 16 completed

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,18 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function() 
 
 // Do not edit below this line
 module.exports = findTheOldest;
+ 
+
+
+let min = function(a,b) {
+    if( a < b) {
+    return a;
+} else {
+    return b;
+}
+}; 
+
+console.log(min(2, 5));
+console.log(min(3, -1));
+console.log(min(1, 1));
